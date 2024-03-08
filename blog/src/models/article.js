bd8fd5e00b11deb90fdf2198810b561f04fd8126
Ali Mousavi <set.ali.mousavi@gmail.com>
2024-03-08 11:15:47 +0330

    Store

--- blog/src/models/article.js
+++ blog/src/models/article.js
@@ -1,75 +1,9 @@
-import fs from 'fs'
-import path from 'path'
+import { BaseModel, init } from '../store'
 
-const articles = []
+class Article extends BaseModel {
+  static fields = ['title', 'text']
 
-function getFilePath () {
-  return path.resolve(__dirname, 'article.data')
+  static entityName = 'articles'
 }
 
-function getData () {
-  const filePath = getFilePath()
-  const data = fs.readFileSync(filePath, 'utf-8')
-
-  return data ? JSON.parse(data) : []
-}
-
-function saveData (data) {
-  const filePath = getFilePath()
-  fs.writeFileSync(filePath, JSON.stringify(data))
-}
-
-export default class Article {
-  constructor ({ id, title, text }) {
-    this.id = id
-    this.title = title
-    this.text = text
-  }
-
-  save () {
-    const articles = getData()
-
-    if (this.id) {
-      const article = articles.find(el => el.id === this.id)
-      article.title = this.title
-      article.text = this.text
-    } else {
-      articles.push({
-        id: Date.now(),
-        title: this.title,
-        text: this.text
-      })
-    }
-
-    saveData(articles)
-  }
-
-  static findAll () {
-    return getData()
-  }
-
-  static find (id) {
-    const articles = getData()
-
-    const article = articles.find(el => el.id === id)
-
-    return article ? new Article(article) : undefined
-  }
-
-  static remove (id) {
-    const articles = getData()
-
-    const articleIndex = articles.findIndex(el => el.id === id)
-
-    if (articleIndex >= 0) {
-      articles.splice(articleIndex, 1)
-      saveData(articles)
-    }
-  }
-
-  remove () {
-    if (this.id) {
-      Article.remove(this.id)
-    }
-  }
-}
+export default init(Article)
---
+++ blog/src/store/index.js
@@ -0,0 +1,114 @@
+import fs from 'fs'
+import path from 'path'
+
+export class BaseModel {
+  constructor ({ id, ...params }) {
+    this.id = id
+
+    this.fields.forEach(field => {
+      this[field] = params[field]
+    })
+  }
+
+  get fields () {
+    return this.constructor.fields
+  }
+
+  get entityName () {
+    return this.constructor.entityName
+  }
+
+  static get data () {
+    return getData(this.entityName)
+  }
+
+  get data () {
+    return this.constructor.data
+  }
+
+  static set data (content) {
+    saveData(this.entityName, content)
+  }
+
+  set data (content) {
+    this.constructor.data = content
+  }
+
+  save () {
+    const data = this.data
+
+    if (this.id) {
+      const entity = data.find(el => el.id === this.id)
+      this.fields.forEach(field => {
+        entity[field] = this[field]
+      })
+    } else {
+      const entity = { id: Date.now() }
+
+      this.fields.forEach(field => {
+        entity[field] = this[field]
+      })
+
+      data.push(entity)
+    }
+
+    this.data = data
+  }
+
+  static findAll () {
+    return this.data
+  }
+
+  static find (id) {
+    const data = this.data.find(el => el.id === id)
+
+    return data ? new this(data) : undefined
+  }
+
+  static remove (id) {
+    const data = this.data
+
+    const dataIndex = data.findIndex(el => el.id === id)
+
+    if (dataIndex >= 0) {
+      data.splice(dataIndex, 1)
+      this.data = data
+    }
+  }
+
+  remove () {
+    if (this.id) {
+      this.constructor.remove(this.id)
+    }
+  }
+}
+
+function getFilePath (entityName) {
+  return path.resolve(__dirname, `${entityName}.data`)
+}
+
+function getData (entityName) {
+  const filePath = getFilePath(entityName)
+  const data = fs.readFileSync(filePath, 'utf-8')
+
+  return data ? JSON.parse(data) : []
+}
+
+function saveData (entityName, data) {
+  const filePath = getFilePath(entityName)
+  fs.writeFileSync(filePath, JSON.stringify(data))
+}
+
+export function init (Entity) {
+  if (!Entity.entityName) {
+    throw new Error('Entity needs entityName static property')
+  }
+
+  const filePath = getFilePath(Entity.entityName)
+
+  if (!fs.existsSync(filePath)) {
+    saveData(Entity.entityName, [])
+  }
+
+  return Entity
+}
